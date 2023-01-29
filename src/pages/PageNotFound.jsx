cda77b77cf537f7e416fab283fb7cfec67835546
askar <mohamedaskar476@gmail.com>
2023-01-29 19:54:50 +0530

    More animated 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import img from "../images/404-Error.gif"
+import entranceGif from "../images/404-Error-entrance.gif"
+import floatingGif from "../images/404-Error-floating.gif"
 
 const PageNotFound = () => {
+    // Two Gifs, one for entrance animation, another on for floating animation
+    // at first, entrance animation gif will be loaded
+    const [image, setImage] = useState(entranceGif)
+
+    useEffect(()=> {
+        // After 1 sec change the gif to floating animation
+        setTimeout(()=>{
+            setImage(floatingGif);
+        },1000)
+    },[])
+
     return ( 
         <div className="container-404">
-            <img src={img} alt="404 - Error: Page not found!!" />
+            <img src={image} alt="404 - Error: Page not found!!" />
             <span>Back to <Link to="/">Home</Link></span>
         </div>
      );
