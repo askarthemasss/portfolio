5595456770cc20217cbd89034fa5a020956c7d12
askar <mohamedaskar476@gmail.com>
2023-02-12 18:22:35 +0530

    Customized About me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,25 +4,36 @@ import Image from "../documents/askar-profile.jpeg"
 
 const About = () => {
     return ( 
-        <div className="about-me">
-            <h1>I'm Askar</h1>
-            <p className="about-me-bio">
-                A Professional, Self-taught Software Developer (specializing in React Applications)
-                with almost 2 Years of Industrial Experience.
-
-                I don't limit myself within a particular tech stack, I'm more of a Problem Solver.
-                I learnt (and still learning) what the problem requires to solve.
-            </p>
-            <SocialLinks gap={"2rem"} />
-            <p className="resume">View My <a href={Resume}>Resume</a></p>
-            <img className="profile-image" src={Image} alt="My Profile" />
-            <br /><br />
-            <ul>
-                <li>Add services - zoho, low-code, Frontend,...</li>
-                <li>Add Tech stack</li>
-                <li>Try adding github stats</li>
-            </ul>
-        </div>
+        <>
+            <div className="about-me-main">
+                <div className="about-me-left">
+                    <h1>I'm Askar</h1>
+                    <p className="about-me-bio">
+                        ‣ A Professional, Self-taught Software Developer (specializing in React Applications)
+                        with almost 2 Years of Industrial Experience. Worked on SaaS customization and
+                        Implementations.
+                        <br /><br />
+                        ‣ I don't limit myself within a particular tech stack, I'm more of a Problem Solver.
+                        I learnt (and still learning) what the problem requires to solve.
+                        <br /><br />
+                        ‣ View My <a href={Resume}>Resume</a>.
+                    </p>
+                    <SocialLinks gap={"3rem"} />
+                    <br /><br />
+                    <ul>
+                        <li>Add services - zoho, low-code, Frontend,...</li>
+                        <li>Add Tech stack</li>
+                        <li>Try adding github stats</li>
+                    </ul>
+                </div>
+                <div className="about-me-right">
+                    <img className="profile-image" src={Image} alt="My Profile" />
+                </div>
+            </div>
+            <div className="about-me-footer">
+                
+            </div>
+        </>
      );
 }
  
